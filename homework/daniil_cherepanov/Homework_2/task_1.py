d8773f06db23f281f3d13caffdd8d5de92137409
Daniil Cherepanov <cherepanovda337@gmail.com>
2026-01-22 18:54:05 +0700

    1) В 3 и 4 задание пришлось поискать в интернете и я справился.

---
+++ homework/daniil_cherepanov/Homework_2/task_1.py
@@ -0,0 +1,10 @@
+a = 14
+b = 9
+
+sum_ab = a + b
+diff_ab = a - b
+prod_ab = a * b
+
+print(sum_ab)
+print(diff_ab)
+print(prod_ab)
---
+++ homework/daniil_cherepanov/Homework_2/task_3.py
@@ -0,0 +1,10 @@
+import math
+
+a = 785
+b = 7
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = math.sqrt(a * b)
+
+print(arithmetic_mean)
+print(geometric_mean)
